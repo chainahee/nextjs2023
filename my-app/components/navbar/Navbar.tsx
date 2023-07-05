098daffcc38152f1b7d0a6487b30f5e7aaf9fb53
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-07-05 17:56:46 +0700

    050722

--- my-app/components/navbar/Navbar.tsx
+++ my-app/components/navbar/Navbar.tsx
@@ -1,41 +1,39 @@
 "use client";
-import Link from "next/link";
 import React, { useState } from "react";
-import { AiOutlineBars } from "react-icons/ai";
+import { BsArrowLeftShort, BsSearch } from "react-icons/bs";
+import { CiServer } from "react-icons/ci";
 
 function Navbar() {
-  const navigation = [
-    { id: 1, name: "Dashboard", rout: "/" },
-    { id: 2, name: "Device", rout: "/device" },
-    { id: 3, name: "Employee", rout: "/employee" },
-    { id: 4, name: "About", rout: "/about" },
-    { id: 5, name: "Settings", rout: "/settings" },
-  ];
-
-  const [visiable, setVisiable] = useState(false);
-
+  const [open, setOpen] = useState(true);
   return (
-    <div className="flex justify-between h-24 items-center">
-      <Link href="/" className="md:text-lg font-bold lg:text-2xl">
-        Borrow & Return System
-      </Link>
-      <div className="flex items-center gap-3">
-        {navigation.map((item) => (
-          <Link key={item.id} href={item.rout}>
-            <span className="md:text-sm lg:text-xl hidden lg:block ">
-              {item.name}
-            </span>
-          </Link>
-        ))}
-        <AiOutlineBars className="md:block lg:hidden text-3xl font-bold cursor-pointer text-purple-800" />
-        <button
-          className="p-2 bg-purple-500 text-white cursor-pointer rounded-md md:text-sm lg:text-xl"
-          onClick={() => {
-            console.log("Logged out");
-          }}
+    <div
+      className={`bg-purple-900 h-screen p-5 pt-8 ${
+        open ? "w-72" : "w-20"
+      } duration-300 relative`}
+    >
+      <BsArrowLeftShort
+        className={`text-3xl bg-white text-gray-900 rounded-full absolute -right-3 top-9 border-purple-800 border cursor-pointer ${
+          !open && "rotate-180"
+        }`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="inline-flex items-center">
+        <CiServer
+          className={`bg-amber-300 text-purple-900 text-4xl rounded cursor-pointer font-medium block float-left mr-2 duration-500 ${
+            open && "rotate-[360deg]"
+          }`}
+        />
+        <span
+          className={`text-white origin-left ${
+            !open && "duration-300 scale-0"
+          } whitespace-nowrap`}
         >
-          Logout
-        </button>
+          Borrow & Return Systems
+        </span>
+      </div>
+      <div className="flex items-center rounded-md bg-purple-100 mt-6 px-4 py-2">
+        <BsSearch className="text-purple-800 text-lg float-left cursor-pointer block mr-2" />
+        <input type={"search"} placeholder="Search" className={`text-base w-full text-purple-800 bg-transparent focus:outline-none ${!open && "hidden"}`} />
       </div>
     </div>
   );
