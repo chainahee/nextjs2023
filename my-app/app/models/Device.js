8a0cb4f050daf05c68f777dc5dc1ba7463c75392
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-07-24 10:34:15 +0700

    ช่วงเช้า

--- my-app/app/models/Device.js
+++ my-app/app/models/Device.js
@@ -2,15 +2,15 @@ import mongoose, { Schema } from "mongoose";
 
 const deviceSchema = new Schema(
   {
-    name: String,
-    serial: String,
-    disc: String,
-    brand: { type: Schema.Types.ObjectId, ref: "Brand" },
-    category: { type: Schema.Types.ObjectId, ref: "Category" },
-    startDate: Date,
-    endDate: Date,
-    status: { type: Schema.Types.ObjectId, ref: "StatusDevice" },
-    price: Number,
+    name: { type: String },
+    serial: { type: String },
+    disc: { type: String },
+    brand: [{ type: Schema.Types.ObjectId, ref: "Brand" }],
+    category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+    startDate: { type: Date },
+    endDate: { type: Date },
+    status: [{ type: Schema.Types.ObjectId, ref: "StatusDevice" }],
+    price: { type: Number },
   },
   { timestamps: true, versionKey: false }
 );
