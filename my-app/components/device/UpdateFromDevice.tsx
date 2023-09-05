169a86e59d5c6e0b162aaf036f3b235564a2f17f
Supat1462 <102660629+Supat1462@users.noreply.github.com>
2023-09-05 18:01:58 +0700

    ช่วงเย็น

--- my-app/components/device/UpdateFromDevice.tsx
+++ my-app/components/device/UpdateFromDevice.tsx
@@ -30,6 +30,10 @@ function UpdateFromDevice({
   const [categorys, setCategorys] = useState([]);
   const [statusdevices, setStatusDevices] = useState([]);
 
+  const dateFromMongoDB = new Date("2023-08-09T00:00:00.000Z"); // ตัวอย่างวันที่จาก MongoDB
+  const formattedDate = dateFromMongoDB.toLocaleDateString("en-GB"); // แปลงเป็นรูปแบบ "DD/MM/YYYY"
+  console.log(formattedDate);
+
   useEffect(() => {
     const fetchBrands = async () => {
       try {
@@ -112,6 +116,8 @@ function UpdateFromDevice({
     }
   };
 
+  // console.log(startDate);
+
   return (
     <div>
       <div className="flex items-center justify-center my-2">
@@ -158,7 +164,7 @@ function UpdateFromDevice({
           </div>
         </div>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-2">
             <label
               htmlFor="brand"
@@ -238,7 +244,7 @@ function UpdateFromDevice({
           </div>
         </div>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="sm:col-span-2">
             <label
               htmlFor="price"
@@ -274,10 +280,14 @@ function UpdateFromDevice({
                 value={newStartDate}
                 name="start-date"
                 id="start-date"
+                placeholder="DD/MM/YYYY"
                 onChange={(e) => {
-                  const selectedDate = e.target.value;
-                  const formattedDate = new Date(selectedDate).toISOString().split("T")[0];
+                  const dateFromMongoDB = e.target.value;
+                  const formattedDate =
+                    dateFromMongoDB.toLocaleDateString("en-GB");
                   setStartDate(formattedDate);
+                  console.log(formattedDate);
+                  
                 }}
                 className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -299,7 +309,9 @@ function UpdateFromDevice({
                 id="end-date"
                 onChange={(e) => {
                   const selectedDate = e.target.value;
-                  const formattedDate = new Date(selectedDate).toISOString().split("T")[0];
+                  const formattedDate = new Date(selectedDate)
+                    .toISOString()
+                    .split("T")[0];
                   setEndDate(formattedDate);
                 }}
                 className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -308,7 +320,7 @@ function UpdateFromDevice({
           </div>
         </div>
 
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div className="col-span-full">
             <label
               htmlFor="discription"
@@ -321,7 +333,7 @@ function UpdateFromDevice({
                 name="discription"
                 id="discription"
                 value={newDisc}
-                rows={3}
+                rows={5}
                 onChange={(e) => setDisc(e.target.value)}
                 className="mt-2 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
